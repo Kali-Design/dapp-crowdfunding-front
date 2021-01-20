55dd59454ec2cfaece38321e4208350d6d2e8b83
Kali-Design <hm_leo@hotmail.com>
2021-01-20 19:56:54 +0100

    working progress

--- src/compenents/SpacePersonal/Application.js
+++ src/compenents/SpacePersonal/Application.js
@@ -4,63 +4,81 @@ import { VStack, Heading, Box, Flex, Spacer, Container, Text, Stack, Input, Inpu
 //import { } from 'ethers'
 import { Web3Context } from "web3-hooks"
 
-import DepositCrowdfunding from '../../App'
+
+import { DepositCrowdfundingContext } from '../../App'
 
 function Application() {
   const [web3State, login] = useContext(Web3Context);
-  const DepositCrowdFunding = useContext(DepositCrowdfunding)
-  const [inputDeposit, setInputDeposit]= useState(null)
+  const DepositCrowdFunding = useContext(DepositCrowdfundingContext)
+  const [inputDeposit, setInputDeposit] = useState(null)
+  const [inputWithdraw, setInputWithdraw] = useState(null)
   const handleOnClickDeposit = async () => {
     try {
-      const dps = await web3State.DepositCrowdFunding.Deposit(inputDeposit)
-      const filter = DepositCrowdFunding.filters.amount()
-      DepositCrowdFunding.once(filter)
+      const dps = await DepositCrowdFunding.deposit()
 
     } catch (e) {
     }
+
   }
+
+  const handleOnClickWithdraw = async () => {
+    try {
+
+      const dps1 = await DepositCrowdFunding.withdraw(inputWithdraw)
+      //console.log(dps1)
+    } catch (e) {
+    }
+  }
+  console.log(DepositCrowdFunding)
 
   return (
     <>
       <VStack spacing={10}>
         <Heading mb={50}>Application</Heading>
       </VStack>
-        <Box>
-          <Container>
-            <Flex my={5}>
-              <Text flex='1' textAlign='center'>Staking Balance</Text>
-              <Spacer />
-              <Text flex='1' textAlign='center'>Reward Balance</Text>
-            </Flex>
-            <Flex alignItems="center" mb={5}>
-              <Text flex='1' textAlign='center'> ETH</Text>
-              <Spacer />
-              <Text flex='1' textAlign='center'> ETH</Text>
-            </Flex>
-          </Container>
-        </Box>
-        <VStack mt={10}>
-          <Box borderWidth="1px" p={50} borderRadius={5}>
-            <Flex mb={5}>
-              <Text fontWeight="bold">Stake Token</Text>
-              <Spacer />
-              <Text > Balance : </Text>
-            </Flex>
-            <Stack spacing={4}>
-              <InputGroup size="sm">
-                <Input placeholder="0" />
+      <Box>
+        <Container>
+          <Flex my={5}>
+            <Text flex='1' textAlign='center'>Staking Balance</Text>
+            <Spacer />
+            <Text flex='1' textAlign='center'>Reward Balance</Text>
+          </Flex>
+          <Flex alignItems="center" mb={5}>
+            <Text flex='1' textAlign='center'> ETH</Text>
+            <Spacer />
+            <Text flex='1' textAlign='center'> ETH</Text>
+          </Flex>
+        </Container>
+      </Box>
+      <VStack mt={10}>
+        <Box borderWidth="1px" p={50} borderRadius={5}>
+          <Flex mb={5}>
+            <Text fontWeight="bold">Fund Ether</Text>
+            <Spacer />
+            <Text > Balance : </Text>
+          </Flex>
+          <Stack spacing={4}>
+            <InputGroup size="sm">
+              <Input placeholder="0" />
                 value={inputDeposit} onChange={(e) => { setInputDeposit(e.currentTarget.value) }}
-                <InputRightAddon bg='blue.400' children="ETH" />
-              </InputGroup>
-              <Button colorScheme="blue" textTransform="uppercase" onClick={handleOnClickDeposit}>Stake !!!</Button>
+              <InputRightAddon bg='blue.400' children="ETH" />
+            </InputGroup>
 
-              <Button variant="link" textTransform="uppercase">Un-Stake...</Button>
+            <InputGroup size="sm">
+              <Input placeholder="0" />
+                value={inputWithdraw} onChange={(e) => { setInputWithdraw(e.currentTarget.value) }}
+              <InputRightAddon bg='blue.400' children="ETH" />
+            </InputGroup>
 
-            </Stack>
-          </Box>
-        </VStack>
+            <Button colorScheme="blue" textTransform="uppercase" onClick={handleOnClickDeposit}>Fund</Button>
 
-      
+            <Button variant="link" textTransform="uppercase" onClick={handleOnClickWithdraw}>Refund</Button>
+
+          </Stack>
+        </Box>
+      </VStack>
+
+
     </>
 
   )
